44ddb51252347945726f95eeddd6d6257f27f1a8
admirakeric <admira.keric@hotmail.com>
2024-05-18 12:14:02 +0200

    Homepage images

--- resources/views/public-part/home/includes/widget.blade.php
+++ resources/views/public-part/home/includes/widget.blade.php
@@ -1,5 +1,58 @@
 <div class="wrapper_widget">
     <div class="inner_wrapper_widget">
+        <div class="widget_headings">
+            <h1>
+                {{ __('Dobrodošli') }}
+            </h1>
+            <p>
+                {{ __('Lorem ipsum dolor sit amet, consectetur adipiscing') }}
 
+            </p>
+        </div>
+        <div class="widget_wrap_info">
+            <div class="widget_box">
+                <div class="widget_box_img">
+                    <div class="widget_box_img_inner_wrapper">
+                        <img class="widget_image" src="{{ asset('files/images/default/hm2.jpg') }}" alt="">
+                    </div>
+                </div>
+                <div class="widget_box_text">
+                    <h4>
+                        {{ __('Iznajmljivanje') }}
+                    </h4>
+                    <p>
+                        {{__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.')}}                    </p>
+                </div>
+            </div>
+            <div class="widget_box">
+                <div class="widget_box_img">
+                    <div class="widget_box_img_inner_wrapper">
+                        <img class="widget_image" src="{{ asset('files/images/default/home_4.jpg') }}" alt="">
+                    </div>
+                </div>
+                <div class="widget_box_text">
+                    <h4>
+                        {{ __('Kupovina') }}
+                    </h4>
+                    <p>
+                        {{__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.')}}                    </p>
+                </div>
+            </div>
+            <div class="widget_box">
+                <div class="widget_box_img">
+                    <div class="widget_box_img_inner_wrapper">
+                        <img class="widget_image" src="{{ asset('files/images/default/home_03.jpg') }}" alt="">
+                    </div>
+                </div>
+                <div class="widget_box_text">
+                    <h4>
+                        {{ __('Prodaja') }}
+                    </h4>
+                    <p>
+                        {{__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.')}}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
