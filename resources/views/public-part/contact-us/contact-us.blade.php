b2b108f9cdc41942cf5d3bac88766556d600e059
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-05-20 20:29:52 +0200

    Contact us

--- resources/views/public-part/contact-us/contact-us.blade.php
+++ resources/views/public-part/contact-us/contact-us.blade.php
@@ -1,16 +1,28 @@
 @extends('public-part.layout.layout')
+
 @section('content')
-<div class="contact_us_wrapper">
-    <div class="contact_us_inner_wrapper">
-        <div class="contact_us_header">
-            <img class="contact_us_image" src="{{ asset('files/images/default/sb1234.jpg') }}" alt="">
-        </div>
-        <div class="contact_us_form">
-
-        </div>
-        <div class="contact_us_maps">
-
+    <div class="cover__image">
+        <div class="shadow">
+            <div class="inner_wrapper">
+                <h1>{{ __('Kontaktirajte nas') }}</h1>
+            </div>
         </div>
     </div>
-</div>
+
+    <div class="contact_us_wrapper">
+        <div class="contact_us_inner_wrapper">
+            <div class="cu_i_w_form">
+                <div class="cu_i_w_left">
+
+                </div>
+                <div class="cu_i_w_right">
+
+                </div>
+            </div>
+
+            <div class="cu_i_w_map">
+
+            </div>
+        </div>
+    </div>
 @endsection
