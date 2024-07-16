53d6750d3fbaa36c2e9e54f96d422e7d8e9cac31
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-07-16 18:26:36 +0200

    Meta tags

--- resources/views/public-part/layout/includes/meta-tags.blade.php
+++ resources/views/public-part/layout/includes/meta-tags.blade.php
@@ -8,13 +8,13 @@
 <meta property="og:url" content="@yield('meta_uri', 'http://europlac-nekretnine.com/')">
 <meta property="og:title" content="@yield('meta_title', __('Euro-Plac d.o.o'))">
 <meta property="og:description" content="@yield('meta_desc', __('Olakšajte i ubrzajte prodaju/kupovinu nekretnine uz Euro-Plac d.o.o - Agenciju za poslovanje nekretninama'))">
-<meta property="og:image" content="@yield('meta_img', asset('files/images/default/logo.png'))">
-<meta property="og:image:secure_url" content="@yield('meta_img', asset('files/images/default/logo.png'))">
+<meta property="og:image" content="@yield('meta_img', asset('files/images/slider/picture1.jpg'))">
+<meta property="og:image:secure_url" content="@yield('meta_img', asset('files/images/slider/picture1.jpg'))">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="@yield('meta_uri', 'http://europlac-nekretnine.com/')">
 <meta property="twitter:title" content="@yield('meta_title', __('Euro-Plac d.o.o'))">
 <meta property="twitter:description" content="@yield('meta_desc', __('Olakšajte i ubrzajte prodaju/kupovinu nekretnine uz Euro-Plac d.o.o - Agenciju za poslovanje nekretninama'))">
-<meta property="twitter:image" content="@yield('meta_img', asset('files/images/default/logo.png'))">
-<meta property="twitter:image:secure_url" content="@yield('meta_img', asset('files/images/default/logo.png'))">
+<meta property="twitter:image" content="@yield('meta_img', asset('files/images/slider/picture1.jpg'))">
+<meta property="twitter:image:secure_url" content="@yield('meta_img', asset('files/images/slider/picture1.jpg'))">
